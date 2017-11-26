a9e81a20b291bfe5f609d5ea65984d83e231289a
John D. Fisher <jdfenw@gmail.com>
2017-11-26 12:09:43 -0800

    Home page posts to database

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from lists.models import Item
+
 # Create your views here.
 def home_page(request):
-    """home_page"""
+    """home_page
+
+    Todo
+    ----
+    * Don't save blank items for every request.
+    * Code smell: POST test is too long?
+    * Display multiple items in the table.
+    * Support more than one list!
+
+    Parameters
+    ----------
+    request
+
+    Returns
+        Return is....
+
+    """
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
     return render(request, 'home.html',
                   {'new_item_text': request.POST.get('item_text', ''),
                   })
