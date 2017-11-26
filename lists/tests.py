8acc4c59e33ab367fd820a4a3fd4de7135143118
John D. Fisher <jdfenw@gmail.com>
2017-11-26 11:06:55 -0800

    Add FT for second to-do

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,11 @@ class HomePageTest(TestCase):
     """Docstring for HomePageTest. """
 
     def test_uses_home_template(self):
-        """test_uses_home_template"""
         response = self.client.get('/')
 
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
