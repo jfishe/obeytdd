464d1aeef255816dd1447e022b32fee22afa1cd0
John D. Fisher <jdfenw@gmail.com>
2017-12-29 16:03:12 -0800

    Refactor: URLs for forms x2
    Move code in views + tests.
    Add new URL.

--- lists/views.py
+++ lists/views.py
@@ -26,9 +26,6 @@ def home_page(request):
         Return is....
 
     """
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
@@ -42,3 +39,13 @@ def view_list(request):
     """
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    """TODO: Docstring for new_list.
+    Returns
+    -------
+    TODO
+
+    """
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
