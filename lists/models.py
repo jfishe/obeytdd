10e55891503cfc28206cd5df2ef8344097414070
John D. Fisher <jdfenw@gmail.com>
2018-01-05 12:21:11 -0800

    Adjust model so that items
    Adjust model so that items are associated with different lists.

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,20 @@
+'''
+Django models
+'''
 from django.db import models
 
+
 # Create your models here.
+class List(models.Model):
+
+    """Docstring for List. """
+
+    pass
+
+
 class Item(models.Model):
+
+    """Item Model"""
+
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
--- lists/views.py
+++ lists/views.py
@@ -3,7 +3,7 @@ Django views
 """
 from django.shortcuts import redirect, render  # type: ignore
 
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 
@@ -15,7 +15,6 @@ def home_page(request):
     ----
     * Adjust model so that items are associated with different lists.
     * Add unique URLs for each list.
-    * Add a URL for creating a new list via POST.
     * Add URLs for adding a new item to an existing list via POST.
 
     Parameters
@@ -23,29 +22,19 @@ def home_page(request):
     request
 
     Returns
-        Return is....
+        Return is render of home.html
 
     """
     return render(request, 'home.html')
 
 
 def view_list(request):
-    """TODO: Docstring for view_list.
-
-    Returns
-    -------
-    TODO
-
-    """
+    """TODO: Docstring for view_list."""
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-    """TODO: Docstring for new_list.
-    Returns
-    -------
-    TODO
-
-    """
-    Item.objects.create(text=request.POST['item_text'])
+    """TODO: Docstring for new_list."""
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
